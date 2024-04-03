c12bc8df0f2d468fc1be2b0acc5efe2dd52b891e
Sergey Kuzmin <s.kuzmin@mail.ru>
2024-04-03 19:02:09 +0800

    Initial commit for main branch

--- rails/utils/processing_utils.py
+++
@@ -1,7 +0,0 @@
-from channels.layers import get_channel_layer
-from asgiref.sync import async_to_sync
-
-
-def send_progress_update(message: str) -> None:
-    channel_layer = get_channel_layer()
-    async_to_sync(channel_layer.group_send)("messages", {"type": "send.progress", "message": message})
